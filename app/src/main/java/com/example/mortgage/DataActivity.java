66fe5fef0599cb20749af62117e5b9c2151e400a
Ống Hành <hanhong20212@gmail.com>
2024-03-18 00:47:39 +0700

    first commit

---
+++ app/src/main/java/com/example/mortgage/DataActivity.java
@@ -0,0 +1,45 @@
+package com.example.mortgage;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DataActivity extends AppCompatActivity {
+    public static final String DA = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.w(DA, "Inside DataActivity:onCreate\n");
+        setContentView(R.layout.activity_data);
+    }
+    public void goBack(View v){
+        this.finish();
+    }
+    protected void onStart(){
+        super.onStart();
+        Log.w(DA, "Inside DataActivity:onStart\n");
+    }
+    protected void onRestart(){
+        super.onRestart();
+        Log.w(DA, "Inside DataActivity:onReStart\n");
+    }
+    protected void onResume(){
+        super.onResume();
+        Log.w(DA, "Inside DataActivity:onResume\n" );
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.w(DA, "Inside DataActivity: onPause\n");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.w(DA, "Inside DataActivity:onStop\n" );
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.w(DA, "Inside DataActivity:onDestroy\n");
+    }
+}
